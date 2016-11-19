d98a780cf285aa5631215c34bcb8ddef6527de25
Matti <uusitalo.matti@gmail.com>
2016-11-19 06:41:12 +0200

    reading from page succeeds

--- background.js
+++ background.js
@@ -16,9 +16,15 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 
 prefillOntrail = function () {
-    chrome.tabs.create({
-	'url': 'http://ontrail.net/#addex'
-    });
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+	    console.log(response.duration);
+	});
+    });    
+    chrome.runtime.sendMessage({'action': 'readData'}, function() {});
+//    chrome.tabs.create({
+//	'url': 'http://ontrail.net/#addex'
+//    });
 };
 
 chrome.pageAction.onClicked.addListener(prefillOntrail);
