93db011646deac6db41e99796508a86b6b961058
Matti <uusitalo.matti@gmail.com>
2016-11-19 14:40:14 +0200

    fixes #5

--- ontrail_addex_contentscript.js
+++ ontrail_addex_contentscript.js
@@ -14,7 +14,11 @@ function prefillValues(request, sender, sendResponse) {
 	fillvalue('ex-avghr', request.avgheartrate);
 	
 	// sport selector is a bit more tricky
-	selector = document.querySelector('#s2id_ex-sport > a > span').innerText = request.extype;
+	selector = document.querySelector('#ex-sport')
+	for (var i = 0; i < selector.options.length; i++) {
+		selector.options[i].selected = selector.options[i].value === request.extype;
+	}
+	selector.dispatchEvent(new Event('change'));
 }
 
 chrome.runtime.onMessage.addListener(prefillValues);
