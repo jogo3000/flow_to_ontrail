28af3efe44a6f1c0c78ec2c59cde01aea805c04b
Matti <uusitalo.matti@gmail.com>
2016-11-19 13:24:37 +0200

    fixes #5

--- flow_content_script.js
+++ flow_content_script.js
@@ -1,8 +1,14 @@
+// http://stackoverflow.com/questions/1418050/string-strip-for-javascript
+function trim(s) {
+	return s.replace(/^\s+|\s+$/g, '');
+}
+
 function readValues(data, sender, sendResponse) {
 	response = {
 			duration: document.getElementById('preciseDuration').getAttribute('value'),
 			distance: document.getElementById('preciseDistanceStr').getAttribute('value'),
-			avgheartrate: document.getElementById('BDPHrAvg').innerText
+			avgheartrate: document.getElementById('BDPHrAvg').innerText,
+			extype: trim(document.getElementById('sport-icon-image').getAttribute('title'))
 	};
 	sendResponse(response);
 }
--- ontrail_addex_contentscript.js
+++ ontrail_addex_contentscript.js
@@ -12,6 +12,9 @@ function prefillValues(request, sender, sendResponse) {
 	fillvalue('ex-duration', request.duration.split('.', 1)[0]);  // duration has millisecond precision
 	fillvalue('ex-distance', request.distance);
 	fillvalue('ex-avghr', request.avgheartrate);
+	
+	// sport selector is a bit more tricky
+	selector = document.querySelector('#s2id_ex-sport > a > span').innerText = request.extype;
 }
 
 chrome.runtime.onMessage.addListener(prefillValues);
