e0a7aa70b3ecc1089d0f7faea445eb6377268e88
Matti <uusitalo.matti@gmail.com>
2016-11-20 15:33:56 +0200

    round changed to format distance. uses two point decimal values. fixes #10

--- src/ontrail_addex_contentscript.js
+++ src/ontrail_addex_contentscript.js
@@ -3,34 +3,33 @@
  */
 
 module.exports = {
-		round: round,
-		toOntrailDateString: toOntrailDateString
+	formatDistance : formatDistance,
+	toOntrailDateString : toOntrailDateString
 };
 
-
-function round(v) {
-	var i = Math.round(parseFloat(v) / 10) * 10;
-	return i;
+function formatDistance(v) {
+	var i = parseFloat(v) / 1000;
+	return i.toFixed(2).replace('.', ',') + ' km';
 }
 
 function toOntrailDateString(d) {
 	return d.getDate() + '.' + d.getMonth() + '.' + d.getFullYear();
 }
 
-function fillvalue(id, value)
-{	
+function fillvalue(id, value) {
 	element = document.querySelector('#' + id);
 	element.value = value;
 	element.dispatchEvent(new KeyboardEvent('keyup')); // trigger validation
-	element.previousElementSibling.classList = ['active']; // beta.ontrail.net
+	element.previousElementSibling.classList = [ 'active' ]; // beta.ontrail.net
 }
 
 function prefillValues(request, sender, sendResponse) {
-	fillvalue('ex-duration', request.duration.split('.', 1)[0]);  // duration has millisecond precision
-	fillvalue('ex-distance', round(request.distance));
+	// duration has millisecond precision
+	fillvalue('ex-duration', request.duration.split('.', 1)[0]);
+	fillvalue('ex-distance', formatDistance(request.distance));
 	fillvalue('ex-avghr', request.avgheartrate);
 	fillvalue('ex-date', toOntrailDateString(new Date(request.timestamp)));
-	
+
 	// sport selector is a bit more tricky
 	selector = document.querySelector('#ex-sport');
 	for (var i = 0; i < selector.options.length; i++) {
--- test/test_ontrail_addex_contentscript.js
+++ test/test_ontrail_addex_contentscript.js
@@ -6,17 +6,14 @@ suite('Ontrail contentscript');
 
 test('#round', function() {
 
-	var tests = [ [ '8000', '8000' ], 
-	              [ '8000', '8001' ],
-	              [ '8010', '8005' ],
-	              [ '8000', '7999' ]
-	              ];
+	var tests = [ [ '8,00 km', '8000' ], [ '8,00 km', '8001' ],
+			[ '8,01 km', '8005' ], [ '8,00 km', '7999' ] ];
 
-	tests.forEach(function (test) {
-		assert.equal(test[0], cs.round(test[1]));
+	tests.forEach(function(test) {
+		assert.equal(test[0], cs.formatDistance(test[1]));
 	})
 });
 
-test('#toOntrailDateString', function(){
+test('#toOntrailDateString', function() {
 	assert.equal('11.1.1981', cs.toOntrailDateString(new Date(1981, 1, 11)));
 });
