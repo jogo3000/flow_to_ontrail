1098bba201277f0568011f5afcdd63de90748930
Matti Uusitalo <uusitalo.matti@gmail.com>
2019-03-03 09:25:14 +0200

    Create output directory if needed

--- compile.js
+++ compile.js
@@ -1,21 +1,32 @@
-var browserify = require('browserify');
+/*
+  Source is written in ES6 which does not run on the browser. It needs to be compiled into browser compatible js.
+*/
+
+const browserify = require('browserify');
 const fs = require('fs');
 
-var files = fs.readdirSync('./src/');
-for (var i = 0; i < files.length; i++) {
-	var file = files[i];
-	var b = browserify([file], {
-			basedir: './src/',
-			debug: true});
-	b.bundle().pipe(fs.createWriteStream('build/' + file));
+// Make sure output directory exists
+if (!fs.existsSync('build')) {
+    fs.mkdirSync('build');
+}
+
+// Browserify source files to output directory
+const sources = fs.readdirSync('./src/');
+for (var i = 0; i < sources.length; i++) {
+    var file = sources[i];
+    var b = browserify([file], {
+	basedir: './src/',
+	debug: true});
+    b.bundle().pipe(fs.createWriteStream('build/' + file));
 };
 
+// Copy source directory to output directory
 function copy(srcdir, targetdir) {
-	var files = fs.readdirSync(srcdir);
-	for (var i = 0; i < files.length; i++) {
-		var src = files[i];
-		fs.createReadStream(srcdir + src).pipe(fs.createWriteStream(targetdir + src));
-	}
+    var files = fs.readdirSync(srcdir);
+    for (var i = 0; i < files.length; i++) {
+	var src = files[i];
+	fs.createReadStream(srcdir + src).pipe(fs.createWriteStream(targetdir + src));
+    }
 }
 
 copy('resources/', 'build/');
